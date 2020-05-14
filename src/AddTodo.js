d29399c715426571b262e552cc2d5ec2f15ff2d2
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-14 23:34:55 +0530

    Add files via upload

---
+++ src/AddTodo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export class AddTodo extends Component {
+
+	state = {
+		content: ''
+
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			content: e.target.value
+		})
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.addTodo(this.state);
+
+		this.setState({
+			content: ''
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<label>Add new todo</label>
+						<input type="text" onChange={this.handleChange} value={this.state.contentcls} />
+					</div>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default AddTodo
